b0b7fbde856ecc9d686ac0f827c212c2de336e19
Marinska <Marinska354@users.noreply.github.com>
2018-12-17 14:47:04 +0700

    content

---
+++ persegi panjang.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int p,l,luas;
+	printf("Masukan panjang persegi :");
+	scanf("%d",&p);
+	printf("Masukan lebar persegi :");
+	scanf("%d",&l);
+	luas=p*l;
+	printf("Nilai luas persegi panjang adalah %d",luas);
+}
